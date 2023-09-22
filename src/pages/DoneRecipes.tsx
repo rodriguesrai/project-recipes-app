337602b9da6d013bdcda671cc8c04bc5aa53d2b5
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-09-22 15:14:11 -0300

    req 44-45

--- src/pages/DoneRecipes.tsx
+++ src/pages/DoneRecipes.tsx
@@ -1,7 +1,80 @@
 import React from 'react';
+import shareIcon from '../images/shareIcon.svg';
 
-export default function DoneRecipes() {
+const doneRecipes = [
+  {
+    id: '52771',
+    type: 'meal',
+    nationality: 'Italian',
+    category: 'Vegetarian',
+    alcoholicOrNot: '',
+    name: 'Spicy Arrabiata Penne',
+    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+    doneDate: '23/06/2020',
+    tags: ['Pasta', 'Curry'],
+  },
+  {
+    id: '178319',
+    type: 'drink',
+    nationality: '',
+    category: 'Cocktail',
+    alcoholicOrNot: 'Alcoholic',
+    name: 'Aquamarine',
+    image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+    doneDate: '23/06/2020',
+    tags: [],
+  },
+];
+
+function DoneRecipes() {
   return (
-    <div>DoneRecipes</div>
+    <>
+      <button data-testid="filter-by-all-btn">All</button>
+      <button data-testid="filter-by-meal-btn">Meals</button>
+      <button data-testid="filter-by-drink-btn">Drinks</button>
+
+      {doneRecipes.map((recipe, index) => (
+        <div key={ index }>
+          <img
+            src={ recipe.image }
+            alt={ recipe.name }
+            data-testid={ `${index}-horizontal-image` }
+          />
+          <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+          { doneRecipes[index].type === 'meal' ? (
+            <p
+              data-testid={ `${index}-horizontal-top-text` }
+            >
+              {`${recipe.nationality} - ${recipe.category}`}
+
+            </p>
+          ) : (
+            <p
+              data-testid={ `${index}-horizontal-top-text` }
+            >
+              {recipe.alcoholicOrNot}
+            </p>
+          )}
+          <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+          <button
+            data-testid={ `${index}-horizontal-share-btn` }
+            src={ shareIcon }
+          >
+            <img src={ shareIcon } alt="share" />
+          </button>
+
+          {recipe.tags.map((tagName, tagIndex) => (
+            <p
+              key={ tagIndex }
+              data-testid={ `${index}-${tagName}-horizontal-tag` }
+            >
+              {tagName}
+            </p>
+          ))}
+        </div>
+      ))}
+    </>
   );
 }
+
+export default DoneRecipes;
