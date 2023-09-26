eeb3271ac5da165dc6a831c1061615e93fbdd283
leonardodefendi <leo_defendi@hotmail.com>
2023-09-26 12:18:28 -0300

    Finaliza requisito 53,54, 55

--- src/components/FavoriteRecipes.tsx
+++ src/components/FavoriteRecipes.tsx
@@ -7,6 +7,8 @@ import { FavoriteType } from '../types';
 
 function FavoriteRecipes() {
   const { setFavorites, favorites, handleClickFavorite } = useContext(ContextSearch);
+  const [copied, setCopied] = useState<string | null>(null);
+  const [filtredResults, setFiltredResults] = useState<FavoriteType[]>([]);
   useEffect(() => {
     // if alterado
     if (JSON.parse(localStorage.getItem('favoriteRecipes') as string)) {
@@ -16,43 +18,56 @@ function FavoriteRecipes() {
     }
   }, []);
 
+  const handleClickCopy = async (path: string, id: string) => {
+    const url = `${window.location.origin}/${path}s/${id}`;
+    console.log(url);
+    await navigator.clipboard.writeText(url);
+    setCopied(id);
+  };
+
+  const filterByBtn = (type: string) => {
+    if (type === 'all') {
+      return setFiltredResults([...favorites]);
+    }
+    return setFiltredResults(favorites.filter((favorite) => favorite.type === type));
+  };
   return (
     <>
       <button
         data-testid="filter-by-all-btn"
-
+        onClick={ () => filterByBtn('all') }
       >
         All
 
       </button>
       <button
         data-testid="filter-by-meal-btn"
-
+        onClick={ () => filterByBtn('meal') }
       >
         Meals
 
       </button>
       <button
         data-testid="filter-by-drink-btn"
-
+        onClick={ () => filterByBtn('drink') }
       >
         Drinks
 
       </button>
       <div>
-        {favorites.length > 0 && (favorites.length > 0 ? favorites
+        {favorites.length > 0 && (filtredResults.length > 0 ? filtredResults
           : favorites).map((favorite, index) => (
             <div key={ favorite.id }>
-
-              <img
-                id="imgFolder"
-                src={ favorite.image }
-                alt=""
-                data-testid={ `${index}-horizontal-image` }
-              />
-
-              <p data-testid={ `${index}-horizontal-name` }>{favorite.name}</p>
-
+              <Link to={ `${window.location.origin}/${favorite.type}s/${favorite.id}` }>
+                <img
+                  id="imgFolder"
+                  alt=""
+                  data-testid={ `${index}-horizontal-image` }
+                />
+              </Link>
+              <Link to={ `${window.location.origin}/${favorite.type}s/${favorite.id}` }>
+                <p data-testid={ `${index}-horizontal-name` }>{favorite.name}</p>
+              </Link>
               <p data-testid={ `${index}-horizontal-top-text` }>{favorite.category}</p>
               {favorite.type === 'meal'
                 ? (
@@ -66,15 +81,18 @@ function FavoriteRecipes() {
                     {favorite.alcoholicOrNot}
 
                   </p>)}
+              {copied === favorite.id && <p>Link copied!</p>}
               <button
                 data-testid={ `${index}-horizontal-share-btn` }
                 src={ ShareIcon }
+                onClick={ () => handleClickCopy(favorite.type, favorite.id) }
               >
                 <img src={ ShareIcon } alt="" />
               </button>
               <button
                 data-testid={ `${index}-horizontal-favorite-btn` }
                 src={ BlackHeart }
+                onClick={ () => handleClickFavorite(`${favorite.type}s`, favorite.id) }
               >
                 <img src={ BlackHeart } alt="" />
               </button>
