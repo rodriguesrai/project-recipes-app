b647051519fcf65845af966f5e68a87686d2191f
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-09-26 13:36:06 -0300

    integração com localstorage

--- src/pages/DoneRecipes.tsx
+++ src/pages/DoneRecipes.tsx
@@ -1,37 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import shareIcon from '../images/shareIcon.svg';
-
-const doneRecipes = [
-  {
-    id: '52771',
-    type: 'meal',
-    nationality: 'Italian',
-    category: 'Vegetarian',
-    alcoholicOrNot: '',
-    name: 'Spicy Arrabiata Penne',
-    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-    doneDate: '23/06/2020',
-    tags: ['Pasta', 'Curry'],
-  },
-  {
-    id: '178319',
-    type: 'drink',
-    nationality: '',
-    category: 'Cocktail',
-    alcoholicOrNot: 'Alcoholic',
-    name: 'Aquamarine',
-    image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-    doneDate: '23/06/2020',
-    tags: [],
-  },
-];
 
 function DoneRecipes() {
   const [filter, setFilter] = useState('all');
   const [copiedIndex, setCopiedIndex] = useState<number | null>(null);
+  const [recipes, setRecipes] = useState([]);
 
-  const filteredRecipes = doneRecipes.filter((recipe) => {
+  useEffect(() => {
+    const localStorageData = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+    setRecipes(localStorageData);
+  }, []);
+
+  const filteredRecipes = recipes.filter((recipe) => {
     if (filter === 'all') {
       return true;
     }
