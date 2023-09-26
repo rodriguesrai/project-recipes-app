fdc2fd251700ea10fb7a0caa9da1688aec4baf68
leonardodefendi <leo_defendi@hotmail.com>
2023-09-26 20:43:47 -0300

    Adiciona style a searchabr

---
+++ src/style/SearchBar.styled.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const FormSearchBar = styled.form`
+display: flex;
+flex-direction: column;
+align-items: center;
+margin: 0;
+background-color: #41197F;
+padding-bottom: 15px;
+`;
+
+export const RadiosLabel = styled.label`
+display: flex;
+justify-content: center;
+color: white;
+margin-top: 10px;
+`;
+
+export const ButtonSearchBar = styled.button`
+  margin-top: 5px;
+  width: 50%;
+  border: none;
+  border-radius: 4px;
+  background-color: #FCC436;
+  color: whitesmoke;
+`;
+
+export const InputRadio = styled.input`
+margin-left: 8px;
+margin-right: 2px;
+&:checked{
+  background-color: red;
+}
+`;
+
+export const InputText = styled.input`
+  border: 1px solid grey;
+  border-radius: 4px;
+  width: 100%;
+ `;
